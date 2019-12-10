ad7492cf78782b58b6c641c06b5da986e32590cc
Alonso97 <magomontas56@gmail.com>
2019-12-09 19:52:51 -0600

    Fin de la sección 3

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  anio: numer = new Date().getFullYear();
+  anio: number = new Date().getFullYear();
 
   constructor() { }
 
